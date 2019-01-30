e94b60c615a0592d9749839c4fcccb576cd8aa65
DDvoretskiy <9554555@gmail.com>
2019-01-30 17:37:46 +0300

    Ресторан 22

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,31 +1,37 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Order;
 import com.javarush.task.task27.task2712.kitchen.Waiter;
 import com.javarush.task.task27.task2712.statistic.StatisticManager;
 
 import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Restaurant {
     private static final int ORDER_CREATING_INTERVAL = 100;
-
+    private static final LinkedBlockingQueue<Order> orderQueue = new LinkedBlockingQueue<>();
     public static void main(String[] args) {
         Cook cook = new Cook("Вася");
+        cook.setQueue(orderQueue);
         Cook cook2 = new Cook("Петя");
-        StatisticManager.getInstance().register(cook);
-        StatisticManager.getInstance().register(cook2);
-        OrderManager orderManager = new OrderManager();
+        cook2.setQueue(orderQueue);
         Waiter waiter = new Waiter();
+        cook.addObserver(waiter);
         cook2.addObserver(waiter);
         ArrayList<Tablet> tablets = new ArrayList<>();
         Tablet tablet;
         for (int i = 0; i < 5; i++) {
             tablet = new Tablet(i);
-            tablet.addObserver(orderManager);
+            tablet.setQueue(orderQueue);
             tablets.add(tablet);
         }
+        Thread threadCook = new Thread(cook);
+        Thread threadCook2 = new Thread(cook2);
         Thread thread = new Thread(new RandomOrderGeneratorTask(tablets,ORDER_CREATING_INTERVAL));
         thread.start();
+        threadCook.start();
+        threadCook2.start();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
